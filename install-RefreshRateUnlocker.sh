a6e487d873bb29b674922d96f70c561798789448
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2025-09-07 14:43:20 -0400

    Add files via upload

--- install-RefreshRateUnlocker.sh
+++ install-RefreshRateUnlocker.sh
@@ -2,143 +2,57 @@
 
 clear
 
-echo Refresh Rate Unlocker Script by ryanrudolf
-echo Discord user dan2wik for the idea on overclocking the display panel to 70Hz
+echo Steam Deck Refresh Rate Unlocker / 70Hz LCD mod script  by ryanrudolf
 echo https://github.com/ryanrudolfoba/SteamDeck-RefreshRateUnlocker
 echo YT - 10MinuteSteamDeckGamer
 sleep 2
 
-# define variables here
-steamdeck_model=$(cat /sys/class/dmi/id/board_name | tr '[:upper:]' '[:lower:]')
-gamescope_orig=$PWD/gamescope/3.6.20/gamescope-3.6.20-orig
-gamescope_unlocked=$PWD/gamescope/3.6.20/gamescope-3.6.20-unlocked
-steamos_version=$(cat /etc/os-release | grep -i version_id | cut -d "=" -f2)
-gamescope_orig_md5sum=c522838a242fabe519958903253a2a4c
-gamescope_unlocked_md5sum=641af753637c710f72748d4ec7fb655b
-actual_gamescope_orig_md5sum=$(md5sum $PWD/gamescope/3.6.20/gamescope-3.6.20-orig | cut -d " " -f1)
-actual_gamescope_unlocked_md5sum=$(md5sum $PWD/gamescope/3.6.20/gamescope-3.6.20-unlocked | cut -d " " -f1)
+# define variables here to capture SteamOS version and Steam Deck model
+MODEL=$(cat /sys/class/dmi/id/board_name)
+STEAMOS_VERSION=$(cat /etc/os-release | grep -i version_id | cut -d "=" -f2)
+SUPPORTED_VERSION=3.7.
+LCD_TEMPLATE=/usr/share/gamescope/scripts/00-gamescope/displays/valve.steamdeck.lcd.lua
+SCRIPT_LOCATION=$HOME/.config/gamescope/scripts
+SCRIPT_NAME=valve.steamdeck.lcd.70Hz.lua
 
-# sanity check - make sure this is on LCD model
-if [ $steamdeck_model = "jupiter" ]
+# sanity checks - make sure this is on Valve Steam Deck LCD and running on supported SteamOS version.
+if [[ "$STEAMOS_VERSION" == *"$SUPPORTED_VERSION"* ]] && [ $MODEL = "Jupiter" ]
+
 then
-	echo Steam Deck LCD detected.
+	echo Script is running on $MODEL Steam Deck LCD using SteamOS $STEAMOS_VERSION.
+	echo This is a supported version / configuration - Steam Deck $MODEL and SteamOS $STEAMOS_VERSION.
 else
-	echo Steam Deck OLED detected.
-	echo Script only works on Steam Deck LCD model.
+	echo Script is running on $MODEL Steam Deck LCD using SteamOS $STEAMOS_VERSION.
+	echo This is a NOT a supported version / configuration.
+	echo Script works on Jupiter Steam Deck LCD and SteamOS 3.7.x.
 	exit
 fi
 
-# sanity check - are you running this in Desktop Mode or ssh / virtual tty session?
-xdpyinfo &> /dev/null
+# create gamescope user script directory
+if [ -d $SCRIPT_LOCATION ]
+then
+	echo The location $SCRIPT_LOCATION already exists.
+	echo Copying 70Hz LCD mod.
+else
+	echo The location $SCRIPT_LOCATION does not exist. Creating it now.
+	mkdir -p $HOME/.config/gamescope/scripts
+	echo Copying 70Hz LCD mod.
+fi
+
+
+# copy the existing gamescope template file to the gamescope user script directory
+cp /usr/share/gamescope/scripts/00-gamescope/displays/valve.steamdeck.lcd.lua $SCRIPT_LOCATION/$SCRIPT_NAME
+
+# make the changes via sed string manipulation
+sed -i '/50, 51, 52, 53, 54, 55, 56, 57, 58, 59,/!b;n;c\\t60, 61, 62, 63, 64, 65, 66, 67, 68, 69, \n\t70' $HOME/.config/gamescope/scripts/valve.steamdeck.lcd.70Hz.lua
+
 if [ $? -eq 0 ]
 then
-	echo Script is running in Desktop Mode.
+	echo 70Hz LCD mod successfully installed.
+	echo Returning to Game Mode in 5sec.
+	sleep 5
+	qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
 else
- 	echo Script is NOT running in Desktop Mode.
-  	echo Please run the script in Desktop Mode as mentioned in the README. Goodbye!
-	exit
-fi
-
-# sanity check - make sure this is running on SteamOS 3.6.20
-if [ $steamos_version = 3.6.20 ]
-then
-	echo Script is running on supported SteamOS - $steamos_version.
-else
-	echo SteamOS - $steamos_version detected. This is NOT a supported version of this script!
-	echo Make sure the SteamOS version is at 3.6.20 and run the script again.
-fi
-
-# sanity check - perform md5sum hash check
-if [ $actual_gamescope_orig_md5sum = $gamescope_orig_md5sum ] && \
-       	[ $actual_gamescope_unlocked_md5sum = $gamescope_unlocked_md5sum ]
-then
-	echo Downloaded files contain a valid md5sum hash.
-else
-	echo Hash mismatch - possible corrupt download. Clone the repo again!
-	exit
-fi
-
-# sanity check - make sure sudo password is already set
-if [ "$(passwd --status $(whoami) | tr -s " " | cut -d " " -f 2)" == "P" ]
-then
-	read -s -p "Please enter current sudo password: " current_password ; echo
-	echo Checking if the sudo password is correct.
-	echo -e "$current_password\n" | sudo -S -k ls &> /dev/null
-
-	if [ $? -eq 0 ]
-	then
-		echo Sudo password is good!
-	else
-		echo Sudo password is wrong! Re-run the script and make sure to enter the correct sudo password!
-		exit
-	fi
-else
-	echo Sudo password is blank! Setup a sudo password first and then re-run script!
-	passwd
-	exit
-fi
-
-# sanity checks are all good. lets go!
-# display warning / disclaimer
-zenity --question --title "Steam Deck Refresh Rate Unlocker" --text \
-	"WARNING: This is for educational and research purposes only! \
-	\n\nThe script has been tested with Steam Deck LCD and the original LCD panel. \
-	\nThis may / may not work with and may / may not cause damage to a DeckHD panel. \
-	\nIf you use a DeckHD panel then proceed at your own risk! \
-	\n\nThe author of this script takes no responsibility for any damage. \
-	\n\nDo you agree to the terms and conditions ?" --width 650 --height 75
-			if [ $? -eq 1 ]
-			then
-				echo User pressed NO. Exit immediately.
-				exit
-			else
-				echo User pressed YES. Continue with the script
-			fi
-
-###### Main menu. Ask user to UNINSTALL or INSTALL
-
-Choice=$(zenity --width 700 --height 220 --list --radiolist --multiple --title "Refresh Rate Unlocker - https://github.com/ryanrudolfoba/SteamOS-RefreshRateUnlocker"\
-	--column "Select One" \
-	--column "Refresh Rate Limit" \
-	--column="Description - Read this carefully!"\
-	FALSE INSTALL "Install the 70Hz mod."\
-	FALSE UNINSTALL "Remove the 70Hz mod."\
-	TRUE EXIT "Don't make any changes and exit immediately.")
-
-if [ $? -eq 1 ] || [ "$Choice" == "EXIT" ]
-then
-	echo User pressed CANCEL / EXIT. Make no changes. Exiting immediately.
-	exit
-
-elif [ "$Choice" == "INSTALL" ]
-then
-	echo Installing the 70Hz mod.
-	echo -e "$current_password\n" | sudo -S steamos-readonly disable &> /dev/null
-	echo -e "$current_password\n" | sudo -S cp $gamescope_unlocked /usr/bin/gamescope
-	if [ $? -eq 0 ] && [ $(md5sum /usr/bin/gamescope | cut -d " " -f 1) = $gamescope_unlocked_md5sum ]
-	then
-		echo 70Hz mod successfully installed.
-		echo -e "$current_password\n" | sudo -S steamos-readonly enable &> /dev/null
-		exit
-	else
-		echo Error installing the 70Hz mod.
-		echo -e "$current_password\n" | sudo -S steamos-readonly enable &> /dev/null
-		exit
-	fi
-	
-elif [ "$Choice" == "UNINSTALL" ]
-then
-	echo Removing the 70Hz mod.
-	echo -e "$current_password\n" | sudo -S steamos-readonly disable &> /dev/null
-	echo -e "$current_password\n" | sudo -S cp $gamescope_orig /usr/bin/gamescope
-	if [ $? -eq 0 ] && [ $(md5sum /usr/bin/gamescope | cut -d " " -f 1) = $gamescope_orig_md5sum ]
-	then
-		echo 70Hz mod successfully removed.
-		echo -e "$current_password\n" | sudo -S steamos-readonly enable &> /dev/null
-		exit
-	else
-		echo Error removing the 70Hz mod.
-		echo -e "$current_password\n" | sudo -S steamos-readonly enable &> /dev/null
-		exit
-	fi
+	echo 70Hz LCD mod NOT successfully installed.
+	echo Please open an issue report on my GitHub repo or leave a comment on the YT channel - 10MinuteSteamDeckGamer.
 fi
